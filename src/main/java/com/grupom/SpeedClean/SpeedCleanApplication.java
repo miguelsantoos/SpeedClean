c20cba5bf19b8e0f429927fd86472bb53246a1e0
miguelsantoos <miguel3.santos3@gmail.com>
2024-12-04 08:39:07 -0300

    Initial commit

---
+++ src/main/java/com/grupom/SpeedClean/SpeedCleanApplication.java
@@ -0,0 +1,13 @@
+package com.grupom.SpeedClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedCleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupom/SpeedClean/SpeedCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupom.SpeedClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
