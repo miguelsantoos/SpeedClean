19e0d01621402e8490da61b71e14ed312998a891
miguelsantoos <miguel3.santos3@gmail.com>
2024-12-06 12:44:09 -0300

    :sparkles: feat: Serviço dos cliente e metodo de buscar, deletar e editar

--- src/main/java/com/grupom/SpeedClean/service/UserService.java
+++ src/main/java/com/grupom/SpeedClean/service/UserService.java
@@ -5,6 +5,9 @@ import com.grupom.SpeedClean.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
 
@@ -13,5 +16,26 @@ public class UserService {
 
     public User saveUser(User user) {
        return repository.save(user);
+    }
+
+    public User buscarUser(Long id) {
+        return repository.getById(id);
+    }
+
+    public List<User> buscarTodosUser() {
+        return repository.findAll();
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+
+    public User editarUser(Long id, User userAtualizado) {
+        User userAntigo = repository.getById(id);
+        userAntigo.setEmail(userAtualizado.getEmail());
+        userAntigo.setName(userAtualizado.getName());
+        userAntigo.setTelephone(userAtualizado.getTelephone());
+        userAntigo.setPassword(userAtualizado.getPassword());
+        return repository.save(userAntigo);
     }
 }
